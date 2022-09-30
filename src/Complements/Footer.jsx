0db05fe514a58db3a04b477f5d29e1d2b00ede5b
Brian Hernández <BrianHP30>
2022-09-30 00:59:32 -0500

    Agregar Footer

--- src/Complements/Footer.jsx
+++ src/Complements/Footer.jsx
@@ -1,7 +1,8 @@
+import '../Styles/Footer.css'
+
 function Footer() {
     return ( 
-        <>
-        </>
+        <footer>@Derechos Reservados</footer>
      );
 }
 
--- src/Conteiners/Layout.jsx
+++ src/Conteiners/Layout.jsx
@@ -6,6 +6,7 @@ function Layout({children}) {
         <>
         <Header></Header>
         {children}
+        <Footer></Footer>
         </>
      );
 }
